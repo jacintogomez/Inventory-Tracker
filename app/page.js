2302aa85cf33ffadf110f4b7a57c33c7b03ca870
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-08-27 19:39:43 -0400

    add/remove buttons in stack

--- app/page.js
+++ app/page.js
@@ -117,8 +117,10 @@ export default function Home(){
               <Typography variant='h3' color='#333' textAlign='center'>
                 {quantity}
               </Typography>
-              <Button variant='contained' onClick={()=>{addItem(name)}}>Add</Button>
-              <Button variant='contained' onClick={()=>{removeItem(name)}}>Remove</Button>
+              <Stack direction='row' spacing={2}>
+                <Button variant='contained' onClick={()=>{addItem(name)}}>Add</Button>
+                <Button variant='contained' onClick={()=>{removeItem(name)}}>Remove</Button>
+              </Stack>
             </Box>
           ))}
         </Stack>
