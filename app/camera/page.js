83b8e37fbd54c096e3df2ea01006e046dcaf9ece
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-09-13 02:06:04 -0400

    Recognizes camera input

--- app/camera/page.js
+++ app/camera/page.js
@@ -1,12 +1,14 @@
 'use client';
 import React,{useState,useRef} from 'react';
 import {Camera} from 'react-camera-pro';
-import {Box, Button, Container, Paper} from "@mui/material";
+import {Box, Button, Container, Paper, Typography} from "@mui/material";
 import GenerateSugg from '../utils/vision.js';
+import {OpenAI} from 'openai';
 
 export default function CameraPage(){
     const camera=useRef(null);
     const [image,setimage]=useState(null);
+    const [sug,setsug]=useState('Nothing');
     const styles={
         camcontainer:{
             width:'300px',
@@ -29,6 +31,38 @@ export default function CameraPage(){
             maxWidth:'300px',
         },
     };
+
+    const GenerateSugg=async ()=>{
+        console.log('starting...');
+        const openai=new OpenAI({
+            apiKey:process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+            dangerouslyAllowBrowser:true,
+        });
+        const response=await openai.chat.completions.create({
+            model:'gpt-4o',
+            messages:[
+                {
+                    role:'user',
+                    content:[
+                        {
+                            type:'text',
+                            text:'Find the most prominent object in the image and say what it is in as few words as possible; just giving the name of whatever object it is',
+                        },
+                        {
+                            type:'image_url',
+                            image_url:{
+                                url:image,
+                                detail:'low',
+                            },
+                        },
+                    ],
+                }
+            ]
+        });
+        console.log(response.choices[0].message.content);
+        setsug(response.choices[0].message.content);
+    }
+
     return (
         <Container maxWidth='50vw'>
             <Paper elevation={3} sx={{p:2,mt:2}}>
@@ -38,7 +72,7 @@ export default function CameraPage(){
                     </div>
                     <Box display='flex'>
                         <Button variant='contained' onClick={()=>setimage(camera.current.takePhoto())} sx={{m:2}}>Take Photo</Button>
-                        <Button variant='contained' onClick={()=>GenerateSugg(image)} sx={{m:2}}>Generate</Button>
+                        <Button variant='contained' onClick={GenerateSugg} sx={{m:2}}>Generate</Button>
                     </Box>
                     {image&&(
                         <Box sx={{mt:2,width:'100%',maxWidth:'300px'}}>
@@ -46,6 +80,7 @@ export default function CameraPage(){
                         </Box>
                     )}
                 </Box>
+                <Typography variant='h3'>{sug}</Typography>
             </Paper>
         </Container>
     )
